b38046fdbe8c8c066d212683dacb162a3fcd1117
Caci <caciipakovska@gmail.com>
2017-03-26 00:42:55 +0100

    added VILIPCHE

--- routes.py
+++ routes.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "<h2>Здраво Вилипчеееее</h2>"
+  return "<h2>Здраво Вилиasdпчеееее</h2><br><img src='static\img\\filipche.jpg'/>"
 
 @app.route("/about")
 def about():
   return "Aloha!"
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(host='0.0.0.0')
